85989b50f1f1fef51a7251bbcce2f1a02cfcd9df
Crowdbotics <team@crowdbotics.com>
2023-08-06 02:30:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "scenariominer-42675",
+  url: "https://scenariominer-42675.botics.co",
+  api: "https://scenariominer-42675.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
